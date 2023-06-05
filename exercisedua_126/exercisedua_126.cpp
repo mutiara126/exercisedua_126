3114ce8d88a2547025d48ea4374327e1ab6523a4
mutiara126 <salsamutiara1812@gmail.com>
2023-06-05 11:22:27 +0800

    penerbit game press

--- exercisedua_126/exercisedua_126.cpp
+++ exercisedua_126/exercisedua_126.cpp
@@ -1,7 +1,43 @@
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+class orang {
+public:
+    string nama;
 
+    orang(string pNama) :
+        nama(pNama) {
+        cout << "orang dibuat\n" << endl;
+    }
+    ~orang() {
+        cout << "orang dihapus\n" << endl;
+    }
+
+    int jumlah(int a, int b) {
+        return a + b;
+    }
+
+};
+
+class pengarang : public orang {
+public:
+    string penerbit;
+
+    pengarang(string pNama, string ppenerbit) :
+        orang(pNama),
+        penerbit(ppenerbit) {
+        cout << "pengarang dibuat\n" << endl;
+    }
+    ~pengarang () {
+        cout << "pengarang dihapus\n" << endl;
+    }
+    string perkenalan() {
+        return "Halo, nama saya" + nama + "pengarang" + penerbit + "\n\n";
+    }
+};
+
+int main() {
+    pengarang("Joko", "penerbit Game Press");    //pengarang 1
+    pengarang("Lia", "penerbit Game Press");     //pengarang 2
+    pengarang("Giga", "penerbit Game Press");    //pengarang 3
